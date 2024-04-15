47951230ce5cd01bae1545c55ce77324e07f1c44
Hieu1607 <nguyenhieu16072004@gmail.com>
2024-04-15 16:22:54 +0700

    chieu t2

---
+++ core/src/com/mygdx/game/Fish/Fish_to_eat.java
@@ -0,0 +1,45 @@
+package com.mygdx.game.Fish;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class Fish_to_eat {
+    public static final int SPEED = 150;
+    public static final int WIDTH = 0;
+    private static Texture texture;
+    float x,y;
+    public boolean remove = false;
+    public void show(){
+    }
+
+
+    private int x_summon_location =random.nextInt(2);
+    public Fish_to_eat(int i){
+        int[] x_value_arr = new int[2];
+        x_value_arr[1] = Gdx.graphics.getWidth();
+        x = x_value_arr[x_summon_location];
+        y = random.nextFloat()*(Gdx.graphics.getHeight()-WIDTH);
+        int fish_choose = random.nextInt(5);
+        texture = new Texture("fish"+i+".png");
+
+    }
+
+    public void update(float deltaTime){
+        if(x_summon_location == 0){
+            x += SPEED*deltaTime;
+            if(x>Gdx.graphics.getWidth())
+                remove = true;
+        }else{
+            x-=SPEED*deltaTime;
+            if(x<0)
+                remove = true;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture,x,y,80,80);
+    }
+    }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,15 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Screens.MainMenu;
-import com.mygdx.game.Screens.MainScreen;
 
 public class MyGdxGame extends Game {
 	public SpriteBatch batch;
